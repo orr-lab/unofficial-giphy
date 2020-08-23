de4b969ad75a11d0082c6765d0a36d12963c23c0
orr-lab <67173344+orr-lab@users.noreply.github.com>
2020-08-23 12:33:02 +0300

    Add files via upload

---
+++ TheHeart.py
@@ -0,0 +1,30 @@
+#https://discord.com/api/oauth2/authorize?client_id=745670425811353690&permissions=121920&scope=bot
+
+import discord
+
+import Secret
+
+import gifRender
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    
+@client.event
+async def on_message(message):
+    massage_content = message.content
+    split = massage_content.split()
+    if message.author == client.user:
+        return
+    
+    if split[0] == "g!":
+        await message.channel.send(gifRender
+.gif_link(split[1]))
+        
+        
+
+
+client.run(Secret.TOKEN)
